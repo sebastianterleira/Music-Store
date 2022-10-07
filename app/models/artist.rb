ef072d31e84b408a4090a8ac2cee9221728c3ceb
Alfredo Arroyo <alfredo7891@gmail.com>
2022-10-07 18:48:06 -0500

    UPDATE: wip validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ApplicationRecord
+  validates :name, presence: true
+  validates :birth_date, comparison: { less_than: Time.zone.today,
+    message: "Can't be in the future" }, if: :verify_birth_date?
+  validate :birth_date_should_exist_for_death_date, if: :verify_birth_date?
+  def verify_birth_date?
+    birth_date.present?
+  end
+  def birth_date_should_exist_for_death_date
+    # return if birth_date.present?
+    errors.add(:death_date, "Can't be before your birthdate") if death_date < birth_date
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,5 @@
 class OrderDetail < ApplicationRecord
   # belongs_to :order
   # belongs_to :album_song
+  validates :order_id, :album_song_id, :quantity, presence: true
 end
